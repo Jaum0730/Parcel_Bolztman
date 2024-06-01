a040577e25b3868e1dcb043e2a066615ed2bce35
João Victor Lopes <64561972+Jaum0730@users.noreply.github.com>
2024-06-01 20:08:40 -0300

    feat: ajuste no texto e adição de efeitos no git hub

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -2,39 +2,61 @@ import { Scene } from "phaser";
 
 export class Credits extends Scene {
     constructor() {
-      super("Credits");
+        super("Credits");
     }
-  
+
     create() {
-  
-      // texto de agradecimento ao professor
-      this.text1 = this.add.text(100, 100, "Agradeceço ao professor pela dedicação e conhecimento compartilhados,\nalém da paciência com os meus atrasos.\n", { fontSize: '24px', fill: '#ffffff' });
-  
-      // texto de agradecimento à turma
-      this.text2 = this.add.text(100, 150, "Esse foi um jogo que fiz na minha gradução espero que gostem.\nOs links dos materiais aqui usados estão no respositório do git hub.", { fontSize: '24px', fill: '#ffffff' });
-  
-      // Adicione um botão para voltar ao menu
-      const backButton = this.add.text(100, 450, "<<Voltar para o Menu>>", { fontSize: '24px', fill: '#ffffff' });
-      backButton.setInteractive();
-      backButton.on('pointerdown', () => {
-        this.scene.start("MainMenu");
-      });
-  
-      // animações 
-      this.input.on('gameobjectover', function (pointer, gameObject) {
-        gameObject.setTintFill(0xcf70cf);
-      });
-      this.input.on('gameobjectout', function (pointer, gameObject) {
-        gameObject.clearTint();
-      });
-  
-      this.loadingAnimation = this.tweens.add({
-        targets: [backButton],
-        alpha: 0.5, // Esmace o texto
-        duration: 1500, // Duração da animação em milissegungos
-        ease: 'Sine.easeInOut', // Função de easing
-        loop: -1, // Número de repetições do loop, -1 para indefinido
-        yoyo: true // Efeito yoyo
-      });
+        // Initialize text objects with empty strings
+        this.text1 = this.add.text(100, 100, '', { fontSize: '24px', fill: '#ffffff' });
+        this.text2 = this.add.text(100, 150, '', { fontSize: '24px', fill: '#ffffff' });
+
+        // Define the full text strings
+        this.text1FullString = "Agradeceço ao professor pela dedicação e conhecimentos\ncompartilhados, além da paciência com os meus atrasos.";
+        this.text2FullString = "Esse foi um jogo que fiz na minha gradução espero que gostem.\nOs links dos materiais aqui usados estão no meu\nrespositório do git hub.";
+
+        // Add a button to go back to the menu
+        const backButton = this.add.text(100, 450, "<<Voltar para o Menu>>", { fontSize: '24px', fill: '#ffffff' });
+        backButton.setInteractive();
+        backButton.on('pointerdown', () => {
+            this.scene.start("MainMenu");
+        });
+
+        // Add any animations or visual effects for the prologue
+        this.input.on('gameobjectover', function (pointer, gameObject) {
+            gameObject.setTintFill(0xcf70cf);
+        });
+        this.input.on('gameobjectout', function (pointer, gameObject) {
+            gameObject.clearTint();
+        });
+
+        this.loadingAnimation = this.tweens.add({
+            targets: [backButton],
+            alpha: 0.5,
+            duration: 1500,
+            ease: 'Sine.easeInOut',
+            loop: -1,
+            yoyo: true
+        });
+
+        // Create tweens for the typing effect
+        this.typeText1 = this.tweens.addCounter({
+            from: 0,
+            to: this.text1FullString.length,
+            duration: 5000,
+            onUpdate: this.updateText.bind(this, this.text1, this.text1FullString)
+        });
+
+        this.typeText2 = this.tweens.addCounter({
+            from: 0,
+            to: this.text2FullString.length,
+            duration: 7000,
+            delay: 5500,
+            onUpdate: this.updateText.bind(this, this.text2, this.text2FullString)
+        });
     }
-  }
+
+    updateText(textObject, fullString, tween) {
+        const value = Math.floor(tween.getValue());
+        textObject.setText(fullString.substr(0, value));
+    }
+}
--- src/scenes/Prologue.js
+++ src/scenes/Prologue.js
@@ -2,46 +2,95 @@ import { Scene } from "phaser";
 
 export class Prologue extends Scene {
     constructor() {
-      super('Prologue');
+        super('Prologue');
     }
-  
+
     create() {
-      // Add background and other visual elements for the prologue
-  
-      // Add text and dialogues for the prologue
-      this.text1 = this.add.text(100, 100, "Havia uma época em que o universo estava mergulhado em conflito.", { fontSize: '24px', fill: '#ffffff' });
-      this.text2 = this.add.text(100, 150, "Uma guerra intergaláctica de proporções épicas havia eclodido entre as diferentes facções\nque lutavam pelo controle dos recursos e da supremacia.", { fontSize: '24px', fill: '#ffffff' });
-      this.text3 = this.add.text(100, 200, "Em meio a esse caos, um jovem piloto destemido chamado Max Boltzman emergiu\ncomo uma esperança para a liberdade.\n", { fontSize: '24px', fill: '#ffffff' });
-      this.text4 = this.add.text(100, 250, "Max cresceu em um planeta distante chamado Lagrange,\nonde as naves espaciais eram uma parte intrínseca da cultura.", { fontSize: '24px', fill: '#ffffff' });
-      this.text5 = this.add.text(100, 300, "Desde tenra idade,\nMax sonhava em se tornar um piloto de elite e viajar pelos vastos céus estelares.", { fontSize: '24px', fill: '#ffffff' });
-  
-      // Add a button to go back to the menu
-      const backButton = this.add.text(100, 450, "<<Voltar para o Menu>>", { fontSize: '24px', fill: '#ffffff' });
-      backButton.setInteractive();
-      backButton.on('pointerdown', () => {
-        this.scene.start("MainMenu");
-      });
+        // Add background and other visual elements for the prologue
 
+        // Initialize text objects with empty strings
+        this.text1 = this.add.text(100, 100, '', { fontSize: '24px', fill: '#ffffff' });
+        this.text2 = this.add.text(100, 140, '', { fontSize: '24px', fill: '#ffffff' });
+        this.text3 = this.add.text(100, 220, '', { fontSize: '24px', fill: '#ffffff' });
+        this.text4 = this.add.text(100, 300, '', { fontSize: '24px', fill: '#ffffff' });
+        this.text5 = this.add.text(100, 350, '', { fontSize: '24px', fill: '#ffffff' });
 
-  
-      // Add any animations or visual effects for the prologue
+        // Define the full text strings
+        this.text1FullString = "Havia uma época em que o universo estava mergulhado em conflito.";
+        this.text2FullString = "Uma guerra intergaláctica de proporções épicas havia eclodido\nentre as diferentes facções que lutavam pelo controle\ndos recursos e da supremacia.";
+        this.text3FullString = "Em meio a esse caos, um jovem piloto destemido chamado\nMax Boltzman emergiu como uma esperança para a liberdade.";
+        this.text4FullString = "Max cresceu em um planeta distante chamado Lagrange,\nonde as naves espaciais eram uma parte intrínseca da cultura.";
+        this.text5FullString = "Desde de criança,Max sonhava em se tornar um piloto de elite\ne viajar pelos vastos céus estelares.";
+
+        // Add a button to go back to the menu
+        const backButton = this.add.text(100, 480, "<<Voltar para o Menu>>", { fontSize: '24px', fill: '#ffffff' });
+        backButton.setInteractive();
+        backButton.on('pointerdown', () => {
+            this.scene.start("MainMenu");
+        });
+
+        // Add any animations or visual effects for the prologue
         this.input.on('gameobjectover', function (pointer, gameObject) {
-        gameObject.setTintFill(0xcf70cf);
+            gameObject.setTintFill(0xcf70cf);
         });
         this.input.on('gameobjectout', function (pointer, gameObject) {
-        gameObject.clearTint();
+            gameObject.clearTint();
         });
 
         this.loadingAnimation = this.tweens.add({
             targets: [backButton],
-            alpha: 0.5, // Esmace o texto
-            duration: 1500, // Duração da animação em milisegungos
-            ease: 'Sine.easeInOut', // Easing function
-            loop: -1, // numero de repetições do loop no caso -1 é indefinidament
-            yoyo: true // efeito de yoyo
-            });
-    
-    
+            alpha: 0.5,
+            duration: 1500,
+            ease: 'Sine.easeInOut',
+            loop: -1,
+            yoyo: true
+        });
+
+        // Create tweens for the typing effect
+        this.typeText1 = this.tweens.addCounter({
+            from: 0,
+            to: this.text1FullString.length,
+            duration: 3000,
+            onUpdate: this.updateText.bind(this, this.text1, this.text1FullString)
+        });
+
+        this.typeText2 = this.tweens.addCounter({
+            from: 0,
+            to: this.text2FullString.length,
+            duration: 5000,
+            delay: 4000,
+            onUpdate: this.updateText.bind(this, this.text2, this.text2FullString)
+        });
+
+        this.typeText3 = this.tweens.addCounter({
+          from: 0,
+          to: this.text3FullString.length,
+          duration: 3000,
+          delay: 9000,
+          onUpdate: this.updateText.bind(this, this.text3, this.text3FullString)
+      });
+
+      this.typeText4 = this.tweens.addCounter({
+        from: 0,
+        to: this.text4FullString.length,
+        duration: 3000,
+        delay: 12000,
+        onUpdate: this.updateText.bind(this, this.text4, this.text4FullString)
+    });
+
+    this.typeText5 = this.tweens.addCounter({
+      from: 0,
+      to: this.text5FullString.length,
+      duration: 3000,
+      delay: 15000,
+      onUpdate: this.updateText.bind(this, this.text5, this.text5FullString)
+  });
+
+        
     }
 
-  }
+    updateText(textObject, fullString, tween) {
+        const value = Math.floor(tween.getValue());
+        textObject.setText(fullString.substr(0, value));
+    }
+}
