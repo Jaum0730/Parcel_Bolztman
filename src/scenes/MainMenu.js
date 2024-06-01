9519e98c83aab40c5ad5887b1da9fa5c6f6b420c
João Victor Lopes <64561972+Jaum0730@users.noreply.github.com>
2024-06-01 17:20:28 -0300

    feat: Refatoração de código, ajustes nos nomes dos botões e descontinuação da função displayTopScores
    - Refatoração abrangente do código para melhorar a legibilidade, manutenibilidade e desempenho.
    - Atualização dos nomes de todos os botões para uma nomenclatura mais clara e intuitiva.
    - Remoção da função `displayTopScores` por não ser mais utilizada.

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -28,10 +28,10 @@ export class MainMenu extends Scene
         this.playButton = this.add.text(this.game.renderer.width / 2, 300, 'PLAY', { font:'37px Orbitron', fill: '#f7f2ad' })
         .setOrigin(0.5).setInteractive();  
         
-        this.prologueButton = this.add.text(this.game.renderer.width / 2, 350, 'PROLOGO', { font: '18px Orbitron', fill: '#f7f2ad' })
+        this.prologueButton = this.add.text(this.game.renderer.width / 2, 350, 'PROLOGUE', { font: '18px Orbitron', fill: '#f7f2ad' })
             .setOrigin(0.5).setInteractive();
             
-        this.creditsButton = this.add.text(this.game.renderer.width / 2, 390, '{CREDITOS}', { font: '18px Orbitron', fill: '#f7f2ad' })
+        this.creditsButton = this.add.text(this.game.renderer.width / 2, 390, 'CREDITS', { font: '18px Orbitron', fill: '#f7f2ad' })
             .setOrigin(0.5).setInteractive();
 
         //=================================Colorir Botões do Menu================================//
@@ -48,9 +48,9 @@ export class MainMenu extends Scene
 
 
         
-        this.txtHighScore = this.add.text(this.game.renderer.width / 2, 430, '->>> Top 5:'+ localStorage.getItem('topScores')  +' <<<-', { font: '20px Orbitron', fill: '#f7f2ad' })
+        this.txtHighScore = this.add.text(this.game.renderer.width / 2, 430, '->>> Top Score: '+ localStorage.getItem('topScores')  +' <<<-', { font: '20px Orbitron', fill: '#f7f2ad' })
         .setOrigin(0.5);
-        this.txtHighScore.setTintFill(0xf7f2ad, 0xf7f2ad, 0xbf40bf, 0xbf40bf);*/
+        this.txtHighScore.setTintFill(0xf7f2ad, 0xf7f2ad, 0xbf40bf, 0xbf40bf);
 
         this.playButton.once('pointerdown', function () {
             this.playButton.setTintFill(0xcf70cf);
@@ -91,32 +91,7 @@ export class MainMenu extends Scene
     startCreditos(){
         this.scene.start('Credits');
     }
-    displayTopScores() {
-        let topScores = JSON.parse(localStorage.getItem('topScores')) || [];
-    
-        // Create a container for the top scores table
-        let topScoresContainer = this.add.container(this.game.renderer.width / 2, 430);
-    
-        // Create a table header
-        let tableHeader = this.add.text(0, 0, 'Rank | Player | Score', {
-          fontSize: '18px',
-          fill: '#f7f2ad',
-          align: 'center'
-        });
-        topScoresContainer.add(tableHeader);
-    
-        // Add the top scores to the table
-        let rowY = tableHeader.y + 30;
-        topScores.forEach((score, index) => {
-          let rankText = this.add.text(0, rowY, `${index + 1}.`, { fontSize: '16px', fill: '#f7f2ad' });
-          let scoreText = this.add.text(100, rowY, score.toString(), { fontSize: '16px', fill: '#f7f2ad' });
-          topScoresContainer.add([rankText, scoreText]);
-          rowY += 30;
-        });
-    
-        // Center the top scores container
-        topScoresContainer.x -= topScoresContainer.width / 2;
-      }
+
 
     
 
@@ -160,15 +135,15 @@ export var gameSettings = {
 // Função para inicializar o array de top pontuações
  function initTopScores() {
     if (!localStorage.getItem('topScores')) {
-      localStorage.setItem('topScores', JSON.stringify([]));
+      localStorage.setItem('topScores', 0);
     }
+
   };
   
-  // Função para atualizar o array de top pontuações
+  // Função para atualizar top pontuações na variável local
 export  function updateTopScores(score) {
-    let topScores = JSON.parse(localStorage.getItem('topScores'));
-    topScores.push(score);
-    topScores.sort((a, b) => b - a); // Ordenar em ordem decrescente
-    topScores = topScores.slice(0, 5); // Manter apenas os 5 melhores
-    localStorage.setItem('topScores', JSON.stringify(topScores));
+    const topScores = localStorage.getItem('topScores');
+    if(score > topScores){
+        localStorage.setItem('topScores', score);
+    }
   };
